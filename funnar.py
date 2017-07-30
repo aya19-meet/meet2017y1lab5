7f21382a60aba1bf4dad826e6f64348e8cd6bbb7
aya19-meet <aya19@meet.mit.edu>
2017-07-30 10:54:58 +0200

    Stopping here

--- funnar.py
+++ funnar.py
@@ -1,15 +1,16 @@
 
-def add_numbers():
+def add_numbers(start,end):
     c=0
-    for number in range(num1, num2):
+    for number in range(start,end+1,2):
         c=c+number
-        return c
+    return c
 
-num1 = int(input('give me a number:? '))
-num2 = int( input('give me another number:? '))
+start = int(input('give me a number:? '))
+end=int( input('give me another number:? '))
 
-answer=add_numbers(num1,num2)
+answer=add_numbers(start,end)
 print(answer)
+
 
 
 
---
+++ funnier.py
@@ -0,0 +1,14 @@
+
+#def draw_1d(n,char):
+    #print(char*n)
+#def draw_1d(n,char):
+    #print(char*n)
+#draw_1d(n,char)
+
+def draw_rec(n1,n2):
+    for i in range(n1):
+        print('*'*n2)
+    
+
+
+    
